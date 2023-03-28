c6020129c152a7ac40b2bfcce47100538a6bdeb6
jack-cummings <jmcummi2@ncsu.edu>
2023-03-28 18:20:25 -0400

    add email

--- schedule_job.py
+++ schedule_job.py
@@ -2,13 +2,18 @@ import openai
 import pandas as pd
 import urllib.parse
 import os
+from email.message import EmailMessage
+import smtplib
+from random import randint
 
 '''Core Functions'''
 def get_prompt(inputs):
+    shuffle_int = randint(1,2)
+    wc = inputs[f'wc{shuffle_int}']
     # generate prompt
     prompt = f"You are a social media manager for a {inputs['size']}, {inputs['industry']}" \
              f" company based in {inputs['loc_city']}, {inputs['loc_state']} called {inputs['name']}." \
-             f" Write a tweet that is topical and {inputs['wc']} to be sent as part of a social-media" \
+             f" Write a tweet that is topical and {wc} to be sent as part of a social-media" \
              f" marketing campaign "  # randomly append website link
     return prompt
 
@@ -32,6 +37,22 @@ def get_tweet(prompt):
 
     return text, intent
 
+def sendEmail(text,link):
+    email_address = "johnmcummings3@gmail.com"
+    email_password = os.environ['email_code']
+
+    # create email
+    msg = EmailMessage()
+    msg['Subject'] = f"Tweet This!"
+    msg['From'] = email_address
+    msg['To'] = email_address
+    msg.set_content(f"Tweet This! \n {text} \n Click here to tweet: {link}")
+
+    # send email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
+
 ''' Main Run'''
 # Openai Key
 openai.api_key = os.environ["openai"]
@@ -44,6 +65,7 @@ for id in df['uid'].to_list():
         inputs = temp_df.head(1).to_dict()
         prompt = get_prompt(inputs)
         text, link = get_tweet(prompt)
+        sendEmail(text, link)
         print(text, link)
     else:
         print(f'error: {id}')
