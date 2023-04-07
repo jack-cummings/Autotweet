712c1c8f0ad9734d9bac5c7f3ce45fcde091e2d9
jack-cummings <jmcummi2@ncsu.edu>
2023-04-07 12:31:39 -0400

    add req, drive

--- schedule_job.py
+++ schedule_job.py
@@ -5,8 +5,27 @@ import os
 from email.message import EmailMessage
 import smtplib
 from random import randint
+import gspread
+from io import StringIO
 
 '''Core Functions'''
+def drive_con():
+    creds = {
+      "type": os.environ['g_type'],
+      "project_id": os.environ['g_proj_id'],
+      "private_key_id": os.environ['g_priv_key_id'],
+      "private_key": os.environ['g_priv_key'].replace('\\n', '\n'),
+      "client_email": os.environ['g_client_email'],
+      "client_id": os.environ['g_client_id'],
+      "auth_uri": os.environ['g_auth_uri'],
+      "token_uri": os.environ['g_token_uri'],
+      "auth_provider_x509_cert_url": os.environ['g_auth_prov_cirt'],
+      "client_x509_cert_url": os.environ['g_client_cirt_url'],
+    }
+    sa = gspread.service_account_from_dict(creds)
+    return sa
+
+
 def get_prompt(inputs):
     shuffle_int = randint(1,2)
     wc = inputs[f'wc{shuffle_int}']
@@ -37,35 +56,39 @@ def get_tweet(prompt):
 
     return text, intent
 
-def sendEmail(text,link):
-    email_address = "johnmcummings3@gmail.com"
+def sendEmail(text,link,to_email):
+    from_email_address = "johnmcummings3@gmail.com"
+    to_email_address = to_email
+    print(to_email_address)
     email_password = os.environ['email_code']
 
     # create email
     msg = EmailMessage()
     msg['Subject'] = f"Tweet This!"
-    msg['From'] = email_address
-    msg['To'] = email_address
+    msg['From'] = from_email_address
+    msg['To'] = to_email_address
     msg.set_content(f"Tweet This! \n {text} \n Click here to tweet: {link}")
 
     # send email
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(email_address, email_password)
+        smtp.login(from_email_address, email_password)
         smtp.send_message(msg)
 
 ''' Main Run'''
 # Openai Key
 openai.api_key = os.environ["openai"]
 # Build "db"
-df = pd.read_csv('base_db.csv')
+sa = drive_con()
+sh = sa.open('auto_tweet').sheet1
+df = pd.DataFrame(sh.get_all_records())
 
 for id in df['uid'].to_list():
     temp_df = df[df['uid']==id]
     if temp_df.shape[0]==1:
-        inputs = temp_df.head(1).to_dict()
+        inputs = temp_df.T[id].to_dict()
         prompt = get_prompt(inputs)
         text, link = get_tweet(prompt)
-        sendEmail(text, link)
+        sendEmail(text, link, inputs['email'].strip())
         print(text, link)
     else:
         print(f'error: {id}')
